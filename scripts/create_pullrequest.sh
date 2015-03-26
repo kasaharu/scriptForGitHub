bb371f0aaa135561a375814ee53d3ab8205d7ca8
Wataru.Kasahara <kasahara1019@gmail.com>
2015-03-26 23:23:12 +0900

    Arrange the script for pull request.
    Do not copy file descriptor.
    Assign the argument to the variable.

--- scripts/create_pullrequest.sh
+++ scripts/create_pullrequest.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+ticket_number=$1
+
 if [ $# -ne 1 ]; then
-  echo "指定された引数は$#個です。" 1>&2
-  echo "引数にチケット番号を渡してください。" 1>&2
+  echo "指定された引数は$#個です。"
+  echo "引数にチケット番号を渡してください。"
   exit 1
 fi
 
-if expr "$1" : '[0-9]*' > /dev/null ; then
-  echo "チケット番号 $1 の Pull Request を作成します。" 1>&2
+if expr "$ticket_number" : '[0-9]*' > /dev/null ; then
+  echo "チケット番号 $ticket_number の Pull Request を作成します。"
 else
   echo "チケット番号が不正です。数値を入力してください。"
   exit 1
@@ -15,7 +17,7 @@ fi
 
 
 echo 'Create temporary format file.'
-sed -e "s/ticket-no/$1/g" ~/.pullreqmessage.txt > ~/.pullreqmessage_temp.txt
+sed -e "s/ticket-no/$ticket_number/g" ~/.pullreqmessage.txt > ~/.pullreqmessage_temp.txt
 
 echo 'Exec Pull Request'
 hub pull-request --browse -F ~/.pullreqmessage_temp.txt -b kasaharu:master -h kasaharu:$(git symbolic-ref --short HEAD)
